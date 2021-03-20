73be8e3d595033c16773c6319864be6281591dd4
Shaxadhere <Shaxad.here@gmail.com>
2021-03-20 15:53:26 +0500

    server js

--- server.js
+++ server.js
@@ -1,70 +1,68 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient
-const app = express()
 
-//Connection String
+const app = express();
+
+// Connection String
 const connectionString = "mongodb+srv://admin:dGakD2MeWQprWN02@cluster0.stgq9.mongodb.net/tinderdb?retryWrites=true&w=majority"
 
-MongoClient.connect(connectionString, { useUnifiedTopology: true })
-  .then(client => {
-    console.log('Connected to Database')
-    const db = client.db('star-wars-quotes')
-    const quotesCollection = db.collection('quotes')
+MongoClient.connect(connectionString, {useUnifiedTopology: true}).then(client => {
+    console.log("Connected to database");
+    const db = client.db('Programs')
+    const programCollections = db.collection('programs')
 
     // ========================
     // Middlewares
     // ========================
-    // app.set('view engine', 'ejs')
-    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(bodyParser.urlencoded({extended: true}))
     app.use(bodyParser.json())
-    app.use(express.static('public'))
+    // app.use(express.static('public'))
 
     // ========================
     // Routes
     // ========================
+
     app.get('/', (req, res) => {
-      db.collection('quotes').find().toArray()
-        .then(quotes => {
-          res.render('index.ejs', { quotes: quotes })
+        db.collection('programs').find().toArray().then(programs => {
+            res.status(200).send("Programs API");
         })
-        .catch(/* ... */)
     })
 
-    app.post('/quotes', (req, res) => {
-      quotesCollection.insertOne(req.body)
-        .then(result => {
-          res.redirect('/')
+    app.get('/programs', (req, res) => {
+        db.collection('programs').find().toArray().then(programs => {
+            res.status(200).send(programs);
         })
-        .catch(error => console.error(error))
     })
 
-    app.put('/quotes', (req, res) => {
-      quotesCollection.findOneAndUpdate(
-        { name: 'Yoda' },
-        {
-          $set: {
-            name: req.body.name,
-            quote: req.body.quote
-          }
-        },
-        {
-          upsert: true
-        }
-      )
+    app.post('/programs', (req, res) => {
+        programCollections.insertOne(req.body).then(result => {
+            res.status(201).send(result)
+        }).catch(error => console.error(error))
+    })
+
+    app.put('/programs', (req, res) => {
+        programCollections.findOneAndUpdate({
+            _id: req.body.id
+        }, {
+            $set: {
+                name: req.body.name,
+                type: req.body.type
+            }
+        }, {upsert: true})
         .then(result => res.json('Success'))
         .catch(error => console.error(error))
     })
 
-    app.delete('/quotes', (req, res) => {
-      quotesCollection.deleteOne(
-        { name: req.body.name }
-      )
+    app.delete('/programs', (req, res) => {
+        programCollections.deleteOne(
+            { _id: req.body.id }
+        )
         .then(result => {
-          if (result.deletedCount === 0) {
-            return res.json('No quote to delete')
-          }
-          res.json('Deleted Darth Vadar\'s quote')
+            if(result.deletedCount === 0){
+                return res.json("Nothing to delete with this id")
+            }
+            res.json("Deleted");
         })
         .catch(error => console.error(error))
     })
@@ -75,7 +73,6 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
     const isProduction = process.env.NODE_ENV === 'production'
     const port = isProduction ? 7500 : 3000
     app.listen(port, function () {
-      console.log(`listening on ${port}`)
+        console.log(`listening on port ${port}`)
     })
-  })
-  .catch(console.error)
+}).catch(error => console.error(error))
